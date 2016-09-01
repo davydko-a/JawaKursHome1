efe7455dc577415032b4d991c763a652f3eb9b19
davydko-a <davydko.a@gmail.com>
2016-09-01 15:30:35 +0300

    4 commit

--- src/com/hom1/Main.java
+++ src/com/hom1/Main.java
@@ -10,17 +10,23 @@ public class Main {
     One bum = new One(10, 'h');
     out.println("Первое задание: int = " + bum.getVal() + ", char = " + bum.getSim());
 
+    //Второе задание
      Dog spoot  = new Dog("Spoot", "Ruff");
      Dog scruffy = new Dog("Scruffy", "Wurf");
      out.println("Второе задание: " + spoot.getName() + " says " + spoot.getSays() + ", " + scruffy.getName() + " says " + scruffy.getSays());
 
+        //Третье задание
         out.print("Третье задание: Введите целое число:" );
         Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = a%2;
-        if (b == 0) out.println("Вы ввели четное число");
+        if (in.hasNextInt()) {
+            int a = in.nextInt();
+            int b = a % 2;
+            if (b == 0) out.println("Вы ввели четное число");
             else out.println("Вы ввели не четное число");
+        }
+        else out.println("Вы ввели не целое число");
 
+        //Четвертое задание
         out.print("Четвертое задание: Введите натуральное двузначное число:" );
         Scanner inTwo = new Scanner(System.in);
         String x = inTwo.nextLine();
@@ -32,5 +38,25 @@ public class Main {
             out.println("Сумма цифр двузначного числа:" + z);
         }
             else out.println("Вы ввели не двузначное число");
+
+        //Пятое задание
+        out.println("Пятое задание: Введите два числа:");
+        out.print("Первое число:");
+        Scanner inThird = new Scanner(System.in);
+        if (inThird.hasNextDouble()) {
+            double h = inThird.nextDouble();
+            double p = 10 - h;
+            out.print("Второе число:");
+            Scanner inFourth = new Scanner(System.in);
+            if (inFourth.hasNextDouble()) {
+                double t = inFourth.nextDouble();
+                double k = 10 - t;
+                if (p < k)
+                    out.println("Число " + h + " ближе к 10");
+                else out.println("Число " + t + " ближе к 10");
+            }
+            else out.println("Вы ввели не число");
+        }
+        else out.println("Вы ввели не число");
     }
 }
